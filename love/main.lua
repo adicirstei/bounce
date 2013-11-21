c7ce3088376c23aa425c28009c848e840b34c905
Adrian <adrian@HP-Compaq-6710b-GR679EA-ABB.(none)>
2013-11-21 07:47:35 +0200

    tile loader

--- love/main.lua
+++ love/main.lua
@@ -1,4 +1,21 @@
+local map
+
 function love.load()
+    -- Get Advanced Tiled Loader
+    local ATL = require("modules/AdvTiledLoader") 
 
+    --  Set maps/ as the base directory for map loading
+    ATL.Loader.path = 'maps/'
 
+    -- Use the loader to load the map
+    map = ATL.Loader.load("desert.tmx") 
+
+end
+
+function love.update(dt)
+ 
+end
+ 
+function love.draw()
+    map:draw()
 end
