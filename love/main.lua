09b7cea324826fd6b5e979715a618c6051c52837
Adrian <adicirstei@gmail.com>
2013-11-22 16:00:19 +0200

    ddd

--- love/main.lua
+++ love/main.lua
@@ -1,29 +1,55 @@
 local map
 local tx, ty = 0, 0
 
+-- Get Advanced Tiled Loader
+local ATL = require("modules/AdvTiledLoader") 
+local game = {
+  balls = 3,
+  pad = {
+    x = 2,
+    y = 300,
+    w = 200
+  },
+  update = function(self, dt)
+    if love.keyboard.isDown("left") then self.pad.x = self.pad.x + 250*dt end
+    if love.keyboard.isDown("right") then self.pad.x = self.pad.x - 250*dt end 
+  end,
+  draw = function(self)
+    self.map:draw()
+    self.drawPad()
+    self.drawHUD()
+  end,
+  
+  drawHUD = function()
+  
+  end,
+  
+  drawPad = function()
+  
+  end
+  
+}
+
+
 function love.load()
-    -- Get Advanced Tiled Loader
-    local ATL = require("modules/AdvTiledLoader") 
+
 
     --  Set maps/ as the base directory for map loading
     ATL.Loader.path = 'maps/'
 
     -- Use the loader to load the map
-    map = ATL.Loader.load("desert.tmx") 
+    game.map = ATL.Loader.load("desert.tmx") 
 
 end
 
 function love.update(dt)
-    if love.keyboard.isDown("up") then ty = ty + 250*dt end
-    if love.keyboard.isDown("down") then ty = ty - 250*dt end
-    if love.keyboard.isDown("left") then tx = tx + 250*dt end
-    if love.keyboard.isDown("right") then tx = tx - 250*dt end 
+  game:update(dt)
 end
  
 function love.draw()
-    love.graphics.translate( math.floor(tx), math.floor(ty) )
+    -- love.graphics.translate( math.floor(tx), math.floor(ty) )
     
     -- Set the draw range. Setting this will significantly increase drawing performance.
-    map:autoDrawRange( math.floor(tx), math.floor(ty), 1, pad)    
-    map:draw()
+    -- game.map:autoDrawRange( math.floor(tx), math.floor(ty), 1, pad)    
+    game:draw()
 end
