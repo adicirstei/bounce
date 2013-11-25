6b19941a1e051869334bba7a3c4eed7962da83b9
Adrian <adicirstei@gmail.com>
2013-11-25 12:48:17 +0200

    drawn ball and pad

--- love/main.lua
+++ love/main.lua
@@ -6,13 +6,13 @@ local tiles = {}
 -- Get Advanced Tiled Loader
 local ATL = require("modules/AdvTiledLoader") 
 local pad = {
-  x = 2,
+  x = 200,
   y = 720-16,
   w = 64, 
   h = 16
 }
 local ball = {
-  r = 5
+  r = 6
 }
 
 ball.x=pad.x
@@ -32,7 +32,13 @@ function love.load()
     -- Use the loader to load the map
     game.map = ATL.Loader.load("level1.tmx") 
     game.map("Bricks"):set(0,0,game.map("Bricks")(1, 0))
-
+    
+    pad.left = love.graphics.newImage("pad-left.png")
+    pad.right = love.graphics.newImage("pad-right.png")
+    pad.center = love.graphics.newImage("pad-center.png")
+    
+    ball.img = love.graphics.newImage("solid-ball.png")
+    
     for _, tile in pairs(game.map.tiles) do
       if tile.properties.type then
         tiles[tile.properties.type] = tile
@@ -84,12 +90,18 @@ end
   
 function game:drawPad()
   love.graphics.setColor(255, 255, 255, 255)
-  love.graphics.rectangle("fill", pad.x - pad.w, pad.y, pad.w*2, pad.h)
+  --love.graphics.rectangle("fill", pad.x - pad.w, pad.y, pad.w*2, pad.h)
+  local widthscale = (pad.w * 2-32)/16
+  love.graphics.draw(pad.center, pad.x-pad.w+16, pad.y, 0, widthscale, 1)
+  love.graphics.draw(pad.left, pad.x-pad.w, pad.y)
+  love.graphics.draw(pad.right, pad.x+pad.w-16, pad.y)
 end
 
 function game:drawBall()
   love.graphics.setColor(255, 255, 255, 255)
-  love.graphics.circle("fill", ball.x, ball.y, ball.r, 16)
+  love.graphics.draw(ball.img, ball.x-ball.r, ball.y-ball.r)
+  
+  -- love.graphics.circle("fill", ball.x, ball.y, ball.r, 16)
 end
 
 
